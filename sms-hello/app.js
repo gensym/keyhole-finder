c50a23b6273556a0c20ce166ce9aaa048511bd6a
David Altenburg <david@gensym.org>
2021-10-28 18:46:18 -0700

    initial commit

---
+++ sms-hello/app.js
@@ -0,0 +1,36 @@
+// const axios = require('axios')
+// const url = 'http://checkip.amazonaws.com/';
+let response;
+
+/**
+ *
+ * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
+ * @param {Object} event - API Gateway Lambda Proxy Input Format
+ *
+ * Context doc: https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-context.html 
+ * @param {Object} context
+ *
+ * Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
+ * @returns {Object} object - API Gateway Lambda Proxy Output Format
+ * 
+ */
+exports.receiveSMS = async (event, context) => {
+    try {
+        const messageString = event['Records'][0]['Sns']['Message'];
+        const message = JSON.parse(messageString);
+
+        // const ret = await axios(url);
+        response = {
+            'statusCode': 200,
+            'body': JSON.stringify({
+                body: message['messageBody'],
+                sender: message['originationNumber']
+            })
+        }
+    } catch (err) {
+        console.log(err);
+        return err;
+    }
+
+    return response
+};
